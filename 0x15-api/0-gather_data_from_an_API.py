f29adb4e4e6d0890f3b8df58c9e0c5b048318381
KarenNgugi <karenwngugi12@gmail.com>
2023-01-16 19:57:08 +0300

    made PEP8 corrections

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,24 +8,27 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-  # get employee name
-  emp_id = int(argv[1])
-  emp_data = "https://jsonplaceholder.typicode.com/users/{}".format(emp_id)
-  emp_name = requests.get(emp_data).json()['name']
-  
-  # get total number of tasks
-  emp_tasks_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(emp_id)
-  emp_tasks = requests.get(emp_tasks_url).json()
-  number_tasks = len(emp_tasks)
-  
-  # get number of tasks completed
-  tasks_completed = 0
-  for task in emp_tasks:
-    if task["completed"] == True:
-      tasks_completed += 1
-      
-  # output
-  print("Employee {} is done with tasks ({}/{})".format(emp_name, tasks_completed, number_tasks))
-  for task in emp_tasks:
-    if task["completed"] == True:
-      print(task["title"])
+    # get employee name
+    original_url = "https://jsonplaceholder.typicode.com/users"
+    emp_id = int(argv[1])
+    emp_data = "{}/{}".format(original_url, emp_id)
+    emp_name = requests.get(emp_data).json()['name']
+
+    # get total number of tasks
+    emp_tasks_url = "{}/{}/todos".format(original_utl, emp_id)
+    emp_tasks = requests.get(emp_tasks_url).json()
+    number_tasks = len(emp_tasks)
+
+    # get number of tasks completed
+    tasks_completed = 0
+    for task in emp_tasks:
+        if task["completed"]:
+            tasks_completed += 1
+
+    # output
+    print("Employee {} is done with tasks ({}/{})".format(emp_name,
+                                                          tasks_completed,
+                                                          number_tasks))
+    for task in emp_tasks:
+        if task["completed"]:
+            print(task["title"])
